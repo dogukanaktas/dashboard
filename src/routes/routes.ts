bde984126cb9bd0aafcd55f3417d12757fcbe49c
Dogukan Aktas <dogukan.aktas@gaiabilgi.com>
2021-05-11 15:25:52 +0300

    Remove user component and name attributes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,24 +1,15 @@
 import Admin from "../pages/Admin";
 import Login from "../pages/Login";
-import User from "../pages/User";
 
 const routes = [
   {
     component: Login,
     path: "/login",
-    name: "Login",
-    private: false,
-  },
-  {
-    component: User,
-    path: "/user",
-    name: "User",
     private: false,
   },
   { 
     component: Admin,
     path: "/admin",
-    name: "Admin",
     private: true 
   }
 ];
