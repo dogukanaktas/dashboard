6aec4009a85dd4c658d32efc48f6a2868cbb4d63
Dogukan Aktas <dogukan.aktas@gaiabilgi.com>
2021-05-13 16:09:00 +0300

    Use localStorage for auth instead of isAuth state

--- src/core/PrivateRoute.tsx
+++ src/core/PrivateRoute.tsx
@@ -1,36 +1,18 @@
-import { FC, useContext } from "react";
+import { FC } from "react";
 import { Redirect, Route } from "react-router";
-import { AuthContext } from "../context/AuthContext";
 
-interface PrivateRouteProps  {
-  
-};
+interface PrivateRouteProps {
+  path: string;
+}
 
-const PrivateRoute: FC<PrivateRouteProps> = ({
-  children,
-  ...rest
-}) => {
-  
-  const {isAuth} = useContext(AuthContext)
-  
-  return (
-    <Route
-      render={({ location }) => {
-        if (isAuth) {
-          return children;
-        }
-        console.log(isAuth)
-        return (
-          <Redirect
-            to={{
-              pathname: "/login",
-              state: { from: location },
-            }}
-          />
-        );
-      }}
-    />
-  );
+const PrivateRoute: FC<PrivateRouteProps> = ({ children, path }) => {
+
+  const accessToken = localStorage.getItem("accessToken");
+
+  if (accessToken) {
+    return <Route path={path}>{children}</Route>;
+  }
+  return <Redirect to="/login" />;
 };
 
 export default PrivateRoute;
