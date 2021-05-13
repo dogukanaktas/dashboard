d3efdf9d1a1b4329d01ce7eaff5e0384b6f06a5d
Dogukan Aktas <dogukan.aktas@gaiabilgi.com>
2021-05-13 23:24:35 +0300

    Remove logout function

--- src/services/loginService.tsx
+++ src/services/loginService.tsx
@@ -1,11 +1,6 @@
 import axios from "axios";
 import config from "../env/config";
 
-
-// type IGetToken = (url: string, payload: object) => string;
-
-
-
 const loginService = {
   getToken: async (url: string, payload: object) => {
     const request = await axios.post(config.TOKEN_URL + url, payload);
@@ -15,17 +10,12 @@ const loginService = {
       data: { accessToken },
     } = request;
 
-    console.log(request);
-
     if (status === 200 && statusText === "OK") {
       return accessToken;
     } else {
       console.log(request);
     }
   },
-  logOut: () => {
-    localStorage.removeItem("accessToken");
-  }
 };
 
 export default loginService;
