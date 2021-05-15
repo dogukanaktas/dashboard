1a30cdf57124fcc37043ce57556e761425ff9cf4
Dogukan Aktas <dogukan.aktas@gaiabilgi.com>
2021-05-15 23:11:54 +0300

    Add new form fields

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,11 @@
-import React, { useState, useContext, FC, useEffect } from "react";
-import { Redirect, useLocation } from "react-router";
-import { FormGroup, Input, Label, Button, Form, Alert } from "reactstrap";
+import React, { useState, useContext, FC } from "react";
+import { useHistory } from "react-router";
+import { FormGroup, Input, Label, Button, Form } from "reactstrap";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { AuthContext } from "../context/AuthContext";
 import * as yup from "yup";
+import Header from "../components/Header";
 
 interface LoginProps {}
 interface IFormInputs {
@@ -37,6 +38,7 @@ const Login: FC<LoginProps> = () => {
   } = useForm<IFormInputs>({
     resolver: yupResolver(schema),
   });
+  const history = useHistory();
 
   const onSubmit: SubmitHandler<IFormInputs> = async (formData) => {
     const isValid = await schema.isValid(formData);
@@ -45,36 +47,42 @@ const Login: FC<LoginProps> = () => {
   };
 
   return (
-    <Form onSubmit={handleSubmit(onSubmit)}>
-      <FormGroup row>
-        <Label for="email">Email</Label>
-        <Input
-          type="text"
-          placeholder="email@email.com"
-          id="email"
-          {...register("email")}
-          invalid={isInvalid}
-        />
-        {errors?.email && (
-          <p className="text-danger">{errors?.email?.message}</p>
-        )}
-      </FormGroup>
-      <FormGroup>
-        <Label for="password">Password</Label>
-        <Input
-          type="text"
-          placeholder="password"
-          id="password"
-          {...register("password")}
-        />
-        {errors?.password && (
-          <p className="text-danger">{errors?.password?.message}</p>
-        )}
-      </FormGroup>
-      <Button color="success" disabled={isInvalid}>
-        LOGIN
-      </Button>
-    </Form>
+    <>
+    <Header title="LOGIN"/>
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <FormGroup row>
+          <Label for="email">Email</Label>
+          <Input
+            type="text"
+            placeholder="email@email.com"
+            id="email"
+            {...register("email")}
+            invalid={isInvalid}
+          />
+          {errors?.email && (
+            <p className="text-danger">{errors?.email?.message}</p>
+          )}
+        </FormGroup>
+        <FormGroup>
+          <Label for="password">Password</Label>
+          <Input
+            type="text"
+            placeholder="password"
+            id="password"
+            {...register("password")}
+          />
+          {errors?.password && (
+            <p className="text-danger">{errors?.password?.message}</p>
+          )}
+        </FormGroup>
+        <Button color="success" disabled={isInvalid}>
+          LOGIN
+        </Button>
+        <Button color="danger" onClick={() => history.push('/register')}>
+          REGISTER
+        </Button>
+      </Form>
+    </>
   );
 };
 
