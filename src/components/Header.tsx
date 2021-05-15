84f4e5e060b137dafca7dbeae90e0d32b93a3540
Dogukan Aktas <dogukan.aktas@gaiabilgi.com>
2021-05-15 23:10:13 +0300

    Create header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+interface HeaderProps {
+  title: string;
+}
+
+const Header: FC<HeaderProps> = ({ title }) => {
+  return <h1>{title}</h1>;
+};
+
+export default Header;
