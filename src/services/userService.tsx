e32ee9e35429b78ef192e17c3744bd88375c7d57
Dogukan Aktas <dogukan.aktas@gaiabilgi.com>
2021-05-20 19:39:14 +0300

    Add register function

--- src/services/userService.tsx
+++ src/services/userService.tsx
@@ -1,38 +1,67 @@
 import axios from "axios";
 import config from "../env/config";
+import { EditableFields } from "../pages/User";
+import { Users } from "../types/types";
 import loginService from "./loginService";
 
 const userService = {
-  delete: async (url: string, id: number) => {
-    let data;
-    const request = await axios.delete(config.TOKEN_URL + url + id);
-    const { status, statusText } = request;
-
-    if (status === 200 && statusText === "OK") {
-      data = request.data;
-    } else {
-      console.log(request);
-    }
-
-    return data;
-  },
-
-  getAll: async (url: string, id?: number) => {
-    let data;
+  getAll: async (id?: number) => {
+    let data: Users[] = [];
     await axios
-      .get(config.TOKEN_URL + url + (typeof id === "number" ? id : ""))
+      .get(config.TOKEN_URL + `users/` + (typeof id === "number" ? id : ""))
       .then((res) => {
         const { status, statusText } = res;
 
         if (status === 200 || statusText === "OK") {
           data = res.data;
         } else {
-          console.log(res);
+          console.error(res);
         }
       })
       .catch((err) => console.log(err));
 
     return data;
+  },
+
+  register: async (formData: object) => {
+    let result: string = "";
+    await loginService
+      .getToken("register/", formData)
+      .then((token) => {
+        result = token;
+      })
+      .catch((err) => console.log(err));
+
+    return result;
+  },
+
+  delete: async (id: number | null) => {
+    let data;
+    const request = await axios.delete(
+      config.TOKEN_URL + `users/` + (typeof id === "number" ? `/` + id : ""),
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: localStorage.getItem("accessToken"),
+        },
+      }
+    );
+    const { status, statusText } = request;
+
+    if (status === 200 && statusText === "OK") {
+      data = request.data;
+    } else {
+      console.error(request);
+    }
+
+    return data;
+  },
+
+  patch: async (id: number | null, userData: EditableFields) => {
+    return await axios.patch(
+      config.TOKEN_URL + `users/` + (typeof id === "number" ? `/` + id : ""),
+      userData
+    );
   },
 };
 
